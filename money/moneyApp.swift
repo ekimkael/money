3910911b66ca564309e09fd6163c8443bed6ff11
Ekim Kael <michelmbili@gmail.com>
2022-04-02 19:18:48 +0100

    Initial Commit

---
+++ money/moneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  moneyApp.swift
+//  money
+//
+//  Created by Ekim Kael on 02/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct moneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
